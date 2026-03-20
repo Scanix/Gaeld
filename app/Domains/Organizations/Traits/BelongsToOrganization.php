c75da13426d18d53da9844fa0b17781c5080bc4c
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2026-03-20 10:00:03 +0100

    refactor: verify migration cleanup

---
+++ app/Domains/Organizations/Traits/BelongsToOrganization.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domains\Organizations\Traits;
+
+/** @deprecated Use \App\Support\Traits\BelongsToOrganization instead. */
+trait BelongsToOrganization
+{
+    use \App\Support\Traits\BelongsToOrganization;
+}
