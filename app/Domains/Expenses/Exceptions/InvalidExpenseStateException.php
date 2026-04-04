9358c91ec40b4b0a2d8a8b2f75ef232141e05cb8
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2026-04-04 02:47:10 +0200

    desloppify: standardize DomainException default messages
    Add descriptive default messages to 7 bare exception constructors:
    - UnbalancedEntryException, InvalidEntryDataException
    - AlreadyPostedException, DuplicateReferenceException
    - InvalidExpenseStateException
    - InvalidPaymentException, InvalidInvoiceStateException

--- app/Domains/Expenses/Exceptions/InvalidExpenseStateException.php
+++ app/Domains/Expenses/Exceptions/InvalidExpenseStateException.php
@@ -4,4 +4,10 @@ namespace App\Domains\Expenses\Exceptions;
 
 use App\Support\Exceptions\DomainException;
 
-class InvalidExpenseStateException extends DomainException {}
+class InvalidExpenseStateException extends DomainException
+{
+    public function __construct(string $message = 'Expense is in an invalid state for this operation.', int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
